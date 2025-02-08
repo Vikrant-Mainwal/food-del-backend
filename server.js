62ea6c2ff5a96d9f5ef9a080a988fb337f5076b7
Vikrant Mainwal <142533844+Vikrant-Mainwal@users.noreply.github.com>
2025-02-08 14:15:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -35,8 +35,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
+app.listen();
 
 // mongodb+srv://<username>:<password>@cluster0.9fsr0cr.mongodb.net/?

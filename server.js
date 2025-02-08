b754f49d8a0aadd4bf065a245e7a4d6c8a359d02
Vikrant-Mainwal <vikrantmainwal1301@gmail.com>
2025-02-08 14:20:21 +0530

    minor

--- server.js
+++ server.js
@@ -1,42 +1,37 @@
-import express from "express"
-import cors from "cors"
-import { connectDb } from "./config/db.js"
-import foodRoutes from "./routes/foodRoutes.js"
-import userRoutes from './routes/userRoutes.js';
-import 'dotenv/config'
+import express from "express";
+import cors from "cors";
+import { connectDb } from "./config/db.js";
+import foodRoutes from "./routes/foodRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import "dotenv/config";
 import cartRouter from "./routes/cartRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
 
-// app config
-const app = express()
-// const port = 4000
+// App config
+const app = express();
 
-
-
-// middleware
-app.use(express.json())
+// Middleware
+app.use(express.json());
 app.use(cors({
   origin: ["https://food-del-frontend-ecru.vercel.app"], 
   methods: ["POST", "GET"],
   credentials: true
 }));
-// connection 
+
+// Database connection
 connectDb();
 
-// api endpoint
-app.use('/api/users',userRoutes)
-app.use("/api/food",foodRoutes)
-app.use("/images", express.static('uploads'))
-app.use('/api/cart',cartRouter)
-app.use('/api/order',orderRouter)
+// API Endpoints
+app.use("/api/users", userRoutes);
+app.use("/api/food", foodRoutes);
+app.use("/images", express.static("uploads"));
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
+// Root route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
-
-// mongodb+srv://<username>:<password>@cluster0.9fsr0cr.mongodb.net/?
+// Export app for Vercel
+export default app;

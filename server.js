e4fa815cbb01b2d5f5b307322e69157dfc321456
Vikrant Mainwal <vikrantmainwal1301@gmail.com>
2025-08-09 18:19:53 +0530

    changes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT =  5000;
 // Middleware
 app.use(express.json());
 app.use(cors({ // allow your frontend origin
-  origin: ["http://localhost:5173"], // allow your frontend origin
+  origin: ["https://food-del-frontend-ecru.vercel.app","https://food-del-admin-chi.vercel.app"], // allow your frontend origin
   methods: ["GET", "POST", "PUT", "DELETE"], // include methods you're using
   credentials: true
 }));

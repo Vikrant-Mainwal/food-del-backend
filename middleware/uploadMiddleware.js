8894c45800f415f891d9b30050fb9fcd4b7106c2
Vikrant Mainwal <vikrantmainwal1301@gmail.com>
2025-08-09 18:14:57 +0530

    changes

---
+++ middleware/uploadMiddleware.js
@@ -0,0 +1,5 @@
+import multer from "multer";
+
+const storage = multer.memoryStorage(); // store in memory only
+export const upload = multer({ storage });
+
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { connectDb } from "./config/db.js";
 import foodRoutes from "./routes/foodRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-import "dotenv/config";
 import cartRouter from "./routes/cartRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
 
@@ -14,9 +14,8 @@ const PORT =  5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors({
+app.use(cors({ // allow your frontend origin
   origin: ["http://localhost:5173"], // allow your frontend origin
-  origin: ["http://localhost:5174"], // allow your frontend origin
   methods: ["GET", "POST", "PUT", "DELETE"], // include methods you're using
   credentials: true
 }));
